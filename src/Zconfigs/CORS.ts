0c5cb3f35556d424f59b2c21fb4166420a884903
alexServ <alexdaniel2003.yt@gmail.com>
2025-06-08 19:37:53 -0400

    https localhost disabled

--- src/Zconfigs/CORS.ts
+++ src/Zconfigs/CORS.ts
@@ -1,5 +1,5 @@
 export const cors = {
-  origin: ['https://localhost:3000', 'https://192.168.0.6:3000', 'https://jobget.vercel.app'],
+  origin: ['https://localhost:3000','https://jobget.vercel.app'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   preflightContinue: false, //esto es para que no se detenga en el preflight
   optionsSuccessStatus: 200, // esto es para que no devuelva el status 200
--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
   };
 
   const app = await NestFactory.create(AppModule,{
-    httpsOptions,
+    //httpsOptions,
   });
   const configService = app.get(ConfigService);
 
